b3339fc27a0534e5c1c2dff532a4561da6512f18
Marcus Higgins <halo2305@gmail.com>
2026-09-25 18:35:44 -0500

    some more nonplayer spawning work

--- Scripts/NonPlayerSpawner.cs
+++ Scripts/NonPlayerSpawner.cs
@@ -12,11 +12,11 @@ public partial class NonPlayerSpawner : Node2D
     [Export]
     private CollisionShape2D _activeBounds;
 
+    private Rect2 _spawnArea;
+
     private readonly PackedScene _nonPlayerScene;
 
     private readonly RandomNumberGenerator _random = new();
-
-    
 
     private readonly int _maxNonPlayerCount = 10;
 
@@ -25,36 +25,65 @@ public partial class NonPlayerSpawner : Node2D
     // Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		List<Direction> directions = Enum.GetValues<Direction>().ToList();
+		List<Direction> directions = [.. Enum.GetValues<Direction>()];
 
 		RectangleShape2D activeBoundsRectangle = (RectangleShape2D)_activeBounds.Shape;
 
 		Vector2 size = activeBoundsRectangle.Size;
 		Vector2 center = _activeBounds.GlobalPosition;
 
-		Rect2 spawnArea = new(center - size / 2, size);
+		_spawnArea = new(center - size / 2, size);
 
 	}
     
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        if (_nonPlayerScene is null)
+        if(_nonPlayerScene is null)
         {
             GD.PushError("NonPlayerScene has not been instantiated.");
-            return;
+            throw new NullReferenceException();
         }
 
-        for (int i = _nonPlayerCount; i < _maxNonPlayerCount; i++)
+        if(_nonPlayerCount < _maxNonPlayerCount)
         {
             Node2D nonPlayer = _nonPlayerScene.Instantiate<Node2D>();
 
-            //Vector2 position = new(
-            //    _random.RandfRange(SpawnArea.Position.X, SpawnArea.End.X),
-            //    _random.RandfRange(SpawnArea.Position.Y, SpawnArea.End.Y));
-
-            //nonPlayer.Position = position;
+            nonPlayer.Position = GetSpawnPosition();
             AddChild(nonPlayer);
+            _nonPlayerCount++;
         }
+    }
+
+    private Vector2 GetSpawnPosition()
+    {
+        Rect2? visibleRect = GetCameraVisibleRect();
+        Vector2 position;
+
+        if (visibleRect.Value.HasPoint(position))
+        {
+            position = new Vector2(
+                _random.RandfRange(_spawnArea.Position.X, _spawnArea.End.X),
+                _random.RandfRange(_spawnArea.Position.Y, _spawnArea.End.Y));
+
+        }
+
+        return position;
+    }
+
+    private Rect2? GetCameraVisibleRect()
+    {
+        Camera2D playerCamera = GetViewport().GetCamera2D();
+
+        if (playerCamera is null)
+        {
+            GD.PushError("Player camera has not been instantiated.");
+            throw new NullReferenceException();
+        }
+
+        Vector2 viewportSize = GetViewport().GetVisibleRect().Size;
+        Vector2 halfExtents = viewportSize / 2 / playerCamera.Zoom;
+
+        return new Rect2(playerCamera.GetScreenCenterPosition() - halfExtents, halfExtents * 2);
     }
 }
